0c16afd78ee9ffff63cd1e3ffa071840683a0fc5
Dairon <dairon10_@hotmail.com>
2021-03-24 13:58:31 -0300

    Create the seeds for the Data Base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PhonePlan.create(name: "FaleMais 30", free_time: 30)
+PhonePlan.create(name: "FaleMais 60", free_time: 60)
+PhonePlan.create(name: "FaleMais 120", free_time: 120)
+
+OriginDestinationFare.create(origin: "011", destination: "016", fare: 1.90)
+OriginDestinationFare.create(origin: "016", destination: "011", fare: 2.90)
+OriginDestinationFare.create(origin: "011", destination: "017", fare: 1.70)
+OriginDestinationFare.create(origin: "017", destination: "011", fare: 2.70)
+OriginDestinationFare.create(origin: "011", destination: "018", fare: 0.90)
+OriginDestinationFare.create(origin: "018", destination: "011", fare: 1.90)
