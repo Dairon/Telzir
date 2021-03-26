0eaa7d04f3740229f81408c398519e628856c08d
Dairon <dairon10_@hotmail.com>
2021-03-25 21:30:54 -0300

    Improvements on index

--- app/controllers/telzir_controller.rb
+++ app/controllers/telzir_controller.rb
@@ -1,4 +1,12 @@
 class TelzirController < ApplicationController
+    def index
+        phone_plans = PhonePlan.order(free_time: :asc)
+        @plans = []
+        phone_plans.each do |plan|
+            @plans.append([plan.name])
+        end
+    end
+
     def result
     end
 end
