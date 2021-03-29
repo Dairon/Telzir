9d712266ae5338e72a1ae283806838f897da339c
Dairon <dairon10_@hotmail.com>
2021-03-29 15:21:01 -0300

    Little rework on TelzirController

--- app/controllers/telzir_controller.rb
+++ app/controllers/telzir_controller.rb
@@ -2,6 +2,8 @@ require 'data_base_retrieval'
 
 
 class TelzirController < ApplicationController
+    @@data_base_retrieval = DataBaseRetrieval.new
+    
     def index
         phone_plans = PhonePlan.order(free_time: :asc)
         @plans = []
@@ -15,8 +17,8 @@ class TelzirController < ApplicationController
         @destination = params["destination"]
         @time = params["time"].to_i
         @plan = params["plan"]
-        origin_destination_fare = DataBaseRetrieval.new.search_fare(@origin, @destination)
-        phoneplan = DataBaseRetrieval.new.search_plan(@plan)
+        origin_destination_fare = @@data_base_retrieval.search_fare(@origin, @destination)
+        phoneplan = @@data_base_retrieval.search_plan(@plan)
         telzir_tariff_calculator = TariffCalculator.new(origin_destination_fare, phoneplan, @time)
         @cost_with_plan = telzir_tariff_calculator.cost_with_plan
         @cos_without_plan = telzir_tariff_calculator.cost_without_plan
